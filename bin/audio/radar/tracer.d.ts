79e60bae9a823d2ba840a83ea4a2fc12bdfc174e
André Michelle <andre.michelle@gmail.com>
2022-06-18 19:36:30 +0200

    refresh bin

--- bin/audio/radar/tracer.d.ts
+++
@@ -1,8 +0,0 @@
-import { Obstacle } from "./obstacles.js";
-import { Ray, Touch } from "./ray.js";
-export declare class Tracer {
-    private readonly ray;
-    constructor();
-    trace(obstacles: ReadonlyArray<Obstacle>, position: number, x: number, y: number): Generator<Readonly<Ray>>;
-    step(obstacles: ReadonlyArray<Obstacle>): Touch;
-}
